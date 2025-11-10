83b7ecfbb17d66d665b160855cbc03280d2d9e8c
Laysa Bittencourt <laysaassuncao2015@gmail.com>
2025-11-10 15:46:14 -0300

    tentando botar no bendito mvvm com fragments

---
+++ app/src/main/java/com/example/fisioplac/ui/form_geriatrica/Tela1FichaFragment.kt
@@ -0,0 +1,8 @@
+package com.example.fisioplac.ui.form_geriatrica
+
+import androidx.fragment.app.Fragment
+
+// Adicione ": Fragment()"
+class Tela1FichaFragment : Fragment() {
+    // Por enquanto, fica vazio
+}
